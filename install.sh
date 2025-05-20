98308789cecb47c6191aec1d8fe6eaf42485ed62
almuhdilkarim <almuhdilkarim2@gmail.com>
2025-05-20 20:55:43 +0700

    update

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ if [[ $1 == "install" ]];then
 
     setup_storage_admiral_protocol_fresh
 
-elif [[ $1 === "swipe" ]];then
+elif [[ $1 == "swipe" ]];then
 
     setup_storage_admiral_protocol_swipe
+    
 else
 
     setup_storage_admiral_protocol_reset
