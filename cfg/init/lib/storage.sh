002855b76981f1ac560da5a867338d9cf0e49f3b
almuhdilkarim <almuhdilkarim2@gmail.com>
2025-05-21 09:09:22 +0700

    update

--- cfg/init/lib/storage.sh
+++ cfg/init/lib/storage.sh
@@ -15,10 +15,15 @@ function storage_admiral_formats_luks_partition_keys() {
 
 
 function storage_admiral_formats_luks_partition_root() {  
-    
+
+    if [[ -e  /dev/mapper/lvm_root ]];then
+        yes | lvremove /dev/proc/*
+        yes | vgremove proc
+        yes | pvremove /dev/mapper/lvm_root   
+    fi
+
     echo $STORAGERAND | /usr/bin/cryptsetup luksFormat --batch-mode --type luks2 --sector-size 4096 $DISK_ROOT
     echo $STORAGERAND | /usr/bin/cryptsetup luksAddKey --batch-mode --type luks2 --key-file $STORAGEUNIQ --sector-size 4096 $DISK_ROOT
-    
 }
 
 
