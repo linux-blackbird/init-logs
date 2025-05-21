729080dc437d3bd90a1b6838ef5e3774f96dae3b
almuhdilkarim <almuhdilkarim2@gmail.com>
2025-05-21 10:23:56 +0700

    update

--- cfg/init/lib/storage.sh
+++ cfg/init/lib/storage.sh
@@ -39,9 +39,13 @@ function storage_blackbird_formats_luks_partition_keys() {
 
     if [[ ! -z $DISK_KEYS ]];then
 
-        echo $STORAGERAND | /usr/bin/cryptsetup luksFormat --batch-mode --type luks2 --sector-size 4096 --key-file $STORAGEUNIQ $DISK_KEYS &
+        echo $STORAGERAND | /usr/bin/cryptsetup luksFormat --batch-mode --type luks2 --sector-size 4096 $DISK_KEYS &
         crypting_keys=$!
         wait $crypting_keys
+
+        echo $STORAGERAND | /usr/bin/cryptsetup luksAddKey --batch-mode --key-file $STORAGEUNIQ $DISK_KEYS &
+        phrases_keys=$!
+        wait $phrases_keys
 
     else
         echo 'No directory keys needed'
@@ -51,18 +55,27 @@ function storage_blackbird_formats_luks_partition_keys() {
 
 function storage_blackbird_formats_luks_partition_root() {  
 
-    echo $STORAGERAND | /usr/bin/cryptsetup luksFormat --batch-mode --type luks2 --sector-size 4096 --key-file $STORAGEUNIQ $DISK_ROOT &
+    echo $STORAGERAND | /usr/bin/cryptsetup luksFormat --batch-mode --type luks2 --sector-size 4096 $DISK_ROOT &
     crypting_root=$!
     wait $crypting_root
+
+    echo $STORAGERAND | /usr/bin/cryptsetup luksAddKey --batch-mode --key-file $STORAGEUNIQ $DISK_ROOT &
+    phrases_root=$!
+    wait $phrases_root
     
 }
 
 
 function storage_blackbird_formats_luks_partition_data() {
 
-    echo $STORAGERAND | /usr/bin/cryptsetup luksFormat --batch-mode --type luks2 --sector-size 4096 --key-file $STORAGEUNIQ $DISK_DATA  &
+    echo $STORAGERAND | /usr/bin/cryptsetup luksFormat --batch-mode --type luks2 --sector-size 4096 $DISK_DATA  &
     crypting_data=$!
     wait $crypting_data
+
+
+    echo $STORAGERAND | /usr/bin/cryptsetup luksAddKey --batch-mode --key-file $STORAGEUNIQ $DISK_DATA &
+    phrases_root=$!
+    wait $phrases_root
 }
 
 
