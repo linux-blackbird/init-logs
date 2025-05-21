341e5c4759b5474ac0d2b04ba5a90ff92391b63b
almuhdilkarim <almuhdilkarim2@gmail.com>
2025-05-21 14:41:23 +0700

    update

--- cfg/init/lib/package.sh
+++ cfg/init/lib/package.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-MAIN_KERNELS_PACKAGE="linux-hardened linux-firmware mkinitcpio intel-ucode base base-devel"
+MAIN_KERNELS_PACKAGE="linux-hardened linux-firmware mkinitcpio amd-ucode base base-devel"
 MAIN_NETWORK_PACKAGE="openssh"
 MAIN_SECURED_PAKCAGE="firewalld tang apparmor libpwquality nftables clevis mkinitcpio-nfs-utils luksmeta libpam-google-authenticator"
 MAIN_STORAGE_PACKAGE="xfsprogs lvm2"
 MAIN_TUNNING_PACKAGE="reflector tuned tuned-ppd irqbalance"
-MAIN_UTILITY_PACKAGE="git less btop"
+MAIN_UTILITY_PACKAGE="git less btop qrencode"
 MAIN_DEVELOP_PACKAGE="neovim"
 MAIN_BACKUPS_PACKAGE="rsync"
 MAIN_AUDITOR_PACKAGE=""
